dc649b20e41a19cab49913b1cf587d9cf16067a8
Enni Salmi <enni.salmi@vincit.fi>
2019-01-06 17:40:24 +0100

    Add share functionality for photos

--- src/components/PhotoListItem.js
+++ src/components/PhotoListItem.js
@@ -1,5 +1,23 @@
 import React, { Component } from 'react';
 import { Modal } from 'react-bootstrap'
+import {
+  TwitterShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  RedditShareButton,
+  EmailShareButton,
+  TwitterIcon,
+  TelegramIcon,
+  WhatsappIcon,
+  FacebookIcon,
+  GooglePlusIcon,
+  LinkedinIcon,
+  RedditIcon,
+  EmailIcon
+} from 'react-share';
 
 class PhotoListItem extends Component {
   constructor(props, context) {
@@ -33,6 +51,33 @@ class PhotoListItem extends Component {
             </Modal.Header>
             <Modal.Body>
               <img src={this.props.url} alt={this.props.title}/>
+              <h4 className="share-title">Share link</h4>
+              <div className="share-div">
+                <TwitterShareButton title={this.props.title} url={this.props.url} className="share-icon">
+                  <TwitterIcon size={32} round={true} />
+                </TwitterShareButton>
+                <WhatsappShareButton title={this.props.title} url={this.props.url} className="share-icon">
+                  <WhatsappIcon size={32} round={true} />
+                </WhatsappShareButton>
+                <TelegramShareButton title={this.props.title} url={this.props.url} className="share-icon">
+                  <TelegramIcon size={32} round={true} />
+                </TelegramShareButton>
+                <FacebookShareButton url={this.props.url} className="share-icon">
+                  <FacebookIcon size={32} round={true} />
+                </FacebookShareButton>
+                <GooglePlusShareButton url={this.props.url} className="share-icon">
+                  <GooglePlusIcon size={32} round={true} />
+                </GooglePlusShareButton>
+                <LinkedinShareButton title={this.props.title} url={this.props.url} className="share-icon">
+                  <LinkedinIcon size={32} round={true} />
+                </LinkedinShareButton>
+                <RedditShareButton title={this.props.title} url={this.props.url} className="share-icon">
+                  <RedditIcon size={32} round={true} />
+                </RedditShareButton>
+                <EmailShareButton subject={this.props.title} url={this.props.url} className="share-icon">
+                  <EmailIcon size={32} round={true} />
+                </EmailShareButton>
+              </div>
             </Modal.Body>
           </Modal>
         </div>
