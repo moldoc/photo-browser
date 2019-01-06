a48a8699196f9513b70587508c4e74b4ce3a1a2d
Enni Salmi <enni.salmi@vincit.fi>
2019-01-06 15:08:16 +0100

    Fix navigation styling
    After installing react-boostrap the application had to include the stylesheet for boostrap 3.
    This stylesheet does not support the same classes I had used with boostrap 4, so some changes
    had to be made in order to get the application title and description remain centered.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 const NavBar = () => (
   <section className="jumbotron text-center nav-container">
-    <nav
-      className="navbar navbar-light nav-top"
-    >
+    <nav className="navbar navbar-light nav-title">
       <span className="navbar-brand mb-0 h1">Photo Browser</span>
-      <p className="text-muted"> Browse photos and albums added by the users of this application.
-        Click a photo to see it in full size and to share its url.</p>
     </nav>
+    <p className="text-muted nav-description">
+      Browse photos and albums, click a photo to see it in full size and to share its url.
+    </p>
     <nav className="nav nav-pills nav-fill">
       <a className="nav-item nav-link" href="#">Albums</a>
       <a className="nav-item nav-link active" href="#">Photos</a>
