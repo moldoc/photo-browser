7185e209afbe6f63fa8c9bf6d9a5fee96f52cfff
Enni Salmi <enni.salmi@vincit.fi>
2019-01-14 18:15:09 +0200

    Add ESlint to project

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true
+    },
+    'ecmaVersion': 8,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'prefer-const': 'error'
+  },
+  'parser': 'babel-eslint'
+};
